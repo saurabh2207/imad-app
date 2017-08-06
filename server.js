672e474df50479f03e3a0ef0c025d0dcdc96b5cc
saurabh2207 <30758291+saurabh2207@users.noreply.github.com>
2017-08-06 15:22:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -73,6 +73,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+var articleName = req.params.articleName;
 app.get('/:articleName', function(req, res){
     res.send(path.join(createTemplate[articleName]));
 });
